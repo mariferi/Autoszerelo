a9e77e47d216fc2d82d54ceaed788286dfe6b99b
GyarmatiTamas <gyarmatitamas01@gmail.com>
2021-11-19 18:28:42 +0100

    feat: WebApi_Server/Controllers/ServiceSheetController added, reference to WebApi_Common added

---
+++ WebApi_Common/Models/ServiceSheet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi_Common.Models
+{
+   public class ServiceSheet
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required]
+        [MaxLength(25)]
+        public string CustomerName { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string CarType { get; set; }
+        [Required]
+        [MaxLength(7)]
+        public string LicensePlate { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string ErrorDescription { get; set; }
+
+        public string WorkStatus { get; set; }
+    }
+}
---
+++ WebApi_Server/Controllers/ServiceSheetController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi_Common.Models;
+
+namespace WebApi_Server.Controllers
+{
+    [ApiController]
+    [Route("api/servicesheet")]
+    public class ServiceSheetController : Controller
+    {
+    }
+}
